83f76de309fc404ed9847d61004717e27f910302
run4w4y <add4che@gmail.com>
2019-04-21 17:11:42 +0300

    changed calibration, added sensors reading

--- goalkeeper/calibration.c
+++ goalkeeper/calibration.c
@@ -4,49 +4,53 @@ handle = open.r("cal.txt");
 
 values = new.vector(10, 0);
 
-names[0] = "strlim";
-names[1] = "strmax";
-names[2] = "com_mid";
-names[3] = "down_lt";
-names[4] = "alpha_2";
-names[5] = "alpha_4";
-names[6] = "alpha_6";
-names[7] = "ball_b";
-names[8] = "str_3";
-names[9] = "str_7";
+names[0] = "PORT_SEEKER";
+names[1] = "PORT_COMPASS";
+names[2] = "PORT_LIGHT";
+names[3] = "PORT_BUTTON";
+names[4] = "SEEKER_DELAY";
+names[5] = "COMPASS_DELAY";
+names[6] = "LIGHT_DELAY";
+names[7] = "BUTTON_DELAY";
+names[8] = "LIGHT_COLOR_NUM"; 
+names[9] = "COMPASS_ALPHA";
+names[10] = "SEEKER_DIV";
 
 if (handle == 0) {
-	strlim = 0;
-	strmax = 0;
-	com_mid = 0;
-	down_lt = 0;
-	alpha_2 = 0;
-	alpha_4 = 0;
-	alpha_6 = 0;
-	ball_b = 0;
-	str_3 = 0;
-	str_7 = 0;
+	PORT_SEEKER = 4;
+	PORT_COMPASS = 2;
+	PORT_LIGHT = 1;
+	PORT_BUTTON = 3;
+	SEEKER_DELAY = 50;
+	COMPASS_DELAY = 50;
+	LIGHT_DELAY = 50;
+	BUTTON_DELAY = 50;
+	LIGHT_COLOR_NUM = 1;
+	COMPASS_ALPHA = 100;
+	SEEKER_DIV = 100;
 } else {
-	strlim = tonum(readline(handle));
-	values[0] = strlim;
-	strmax = tonum(readline(handle));
-	values[1] = strmax;
-	com_mid = tonum(readline(handle));
-	values[2] = com_mid;
-	down_lt = tonum(readline(handle));
-	values[3] = down_lt;
-	alpha_2 = tonum(readline(handle));
-	values[4] = alpha_2;
-	alpha_4 = tonum(readline(handle));
-	values[5] = alpha_4;
-	alpha_6 = tonum(readline(handle));
-	values[6] = alpha_6;
-	ball_b = tonum(readline(handle));
-	values[7] = ball_b;
-	str_3 = tonum(readline(handle));
-	values[8] = str_3;
-	str_7 = tonum(readline(handle));
-	values[9] = str_7;
+	PORT_SEEKER = tonum(readline(handle));
+	values[0] = PORT_SEEKER;
+	PORT_COMPASS = tonum(readline(handle));
+	values[1] = PORT_COMPASS;
+	PORT_LIGHT = tonum(readline(handle));
+	values[2] = PORT_LIGHT;
+	PORT_BUTTON = tonum(readline(handle));
+	values[3] = PORT_BUTTON;
+	SEEKER_DELAY = tonum(readline(handle));
+	values[4] = SEEKER_DELAY;
+	COMPASS_DELAY = tonum(readline(handle));
+	values[5] = COMPASS_DELAY;
+	LIGHT_DELAY = tonum(readline(handle));
+	values[6] = LIGHT_DELAY;
+	BUTTON_DELAY = tonum(readline(handle));
+	values[7] = BUTTON_DELAY;
+	LIGHT_COLOR_NUM = tonum(readline(handle));
+	values[8] = LIGHT_COLOR_NUM;
+	COMPASS_ALPHA = tonum(readline(handle));
+	values[9] = COMPASS_ALPHA;
+	SEEKER_DIV = tonum(readline(handle));
+	values[10] = SEEKER_DIV;
 }
 
 closef(handle);
@@ -65,62 +69,12 @@ com_i = 0;
 alpha = com_mid;
 name = getname();
 
-void sensors {
-	while (true) {
-		/* seeker */
-		
-		irseeker_array = i2c.readregs(4, 8, 73, 6);
-		dir1 = irseeker_array[0];
-		str1 = irseeker_array[1];
-		str2 = irseeker_array[2];
-		str3 = irseeker_array[3];
-		str4 = irseeker_array[4];
-		str5 = irseeker_array[5];
-
-		if (rm(dir1, 2) == 0) {
-			strres = (str1 + str2 + str3 + str4 + str5)/1.57;
-		} else {
-			strres = str1 + str2 + str3 + str4 + str5;
-		}
-		
-		if (dir1 == 0 and strres > 100) {
-			dir = 5;
-		} else {
-			if ((dir1 == 3 or dir1 == 7) and strres < 30) {
-				dir = 0;
-			} else {
-				if (dir == 6 and strres > 150) {
-					dir = 5;
-				} else {
-					dir = dir1;
-				}
-			}
-		}
-		
-		/* compass */
-
-		compass_array = i2c.readregs(2, 1, 66, 4);
-		compass = compass_array[0]*2 + compass_array[1];
-		err_com = rm(compass - alpha + 900, 360) - 180;
-		upcom = err_com * com_kp + (err_com - err_com_old) * com_kd;
-		err_com_old = err_com;
-		turn = upcom;
-
-    /* light sensors */
-
-		light_back = sen.percent(1);
-		light_down = sen.percent(3);
-	}
-}
-
 void bottom { // values in the bottom of the screen
 	dotline(1, 0, 105, 178, 105, 4, 4);
 	txt(1, 89 - len(name)*4, 114, 1, name);
 	txt(1, 5, 114, 0, round(voltagern*100)/100);
 	txt.left(1, 173, 114, 0, round(ampersrn()*100)/100);
 }
-
-new.thread = sensors;
 
 // interface
 
@@ -193,7 +147,7 @@ while (true) {
 		
 		i = i - 1;
 		if (i == -1) {
-			i = 9;
+			i = 10;
 		}
 	}
 	
@@ -227,7 +181,7 @@ while (true) {
 		}
 		
 		i = i + 1;
-		if (i == 10) {
+		if (i == 11) {
 			i = 0;
 		}
 	}
@@ -246,15 +200,19 @@ while (true) {
 	}
 	
 	if (btn == "D") { // down button action
-		if (prev_button = "D") {
-			button_counter = button_counter + 1;
+		if (i != ) {
+			if (prev_button = "D") {
+				button_counter = button_counter + 1;
+			} else {
+				button_counter = 0;
+			}
+			if (button_counter < 6) {
+				values[i] = values[i] - 1;
+			} else {
+				values[i] = values[i] - 5;
+			}
 		} else {
-			button_counter = 0;
-		}
-		if (button_counter < 6) {
-			values[i] = values[i] - 1;
-		} else {
-			values[i] = values[i] - 5;
+
 		}
 	}
 	
@@ -272,87 +230,89 @@ while (true) {
 		}
 		
 		if (i == 0) {
-			strmax = 0;
-			for (j = 0; j < 2000; ++j) { // progress bar
-				if (ispressed(6) == true) { // exit button
-					goto end;
+			if (values[i] <= 1) {
+				values[i] = 1;
+			} else {
+				if (values >= 4) {
+					values[i] = 4;
 				}
-				if (str3 > strmax) {
-					strmax = str3;
-				}
-				p = floor(j/20);
-				scr.clear();
-				scr.rect(1, 37, 48, 105, 10);
-				line(1, 39, 50, 39+p, 50);
-				line(1, 39, 51, 39+p, 51);
-				line(1, 39, 52, 39+p, 52);
-				line(1, 39, 53, 39+p, 53);
-				line(1, 39, 54, 39+p, 54);
-				line(1, 39, 55, 39+p, 55);
-				txt(1, 89-len(p)*4, 30, 1, p+"%");
-				delay(5);
 			}
-			values[i] = strmax;
 		}
 		
 		if (i == 1) {
-			values[i] = str3;
+			if (values[i] <= 1) {
+				values[i] = 1;
+			} else {
+				if (values >= 4) {
+					values[i] = 4;
+				}
+			}
 		}
 		
 		if (i == 2) {
-			values[i] = compass;
+			if (values[i] <= 1) {
+				values[i] = 1;
+			} else {
+				if (values >= 4) {
+					values[i] = 4;
+				}
+			}
 		}
 		
 		if (i == 3) {
-			r = 0;
-			g = 0;
-			b = 0;
-			sen.rgb(3, r, g, b);
-			senball_calib = g;
-			values[i] = round(1.25 * senball_calib);
+			if (values[i] <= 1) {
+				values[i] = 1;
+			} else {
+				if (values >= 4) {
+					values[i] = 4;
+				}
+			}
 		}
 		
 		if (i == 4) {
-			values[i] = compass;
+			if (values[i] <= 1) {
+				values[i] = 1;
+			}
 		}
 		
 		if (i == 5) {
-			values[i] = compass;
+			if (values[i] <= 1) {
+				values[i] = 1;
+			}
 		}
 		
 		if (i == 6) {
-			values[i] = compass;
+			if (values[i] <= 1) {
+				values[i] = 1;
+			}
 		}
 		
 		if (i == 7) {
-			senball_calib = light_back;
-			for (j = 0; j < 1000; ++j) { // progress bar
-				if (ispressed(6) == true) { // exit button
-					goto end;
-				}
-				senball_calib = senball_calib + light_back;
-				p = floor(j/10);
-				scr.clear();
-				scr.rect(1, 37, 48, 105, 10);
-				line(1, 39, 50, 39+p, 50);
-				line(1, 39, 51, 39+p, 51);
-				line(1, 39, 52, 39+p, 52);
-				line(1, 39, 53, 39+p, 53);
-				line(1, 39, 54, 39+p, 54);
-				line(1, 39, 55, 39+p, 55);
-				txt(1, 89-len(p)*4, 30, 1, p+"%");
-				delay(5);
+			if (values[i] <= 1) {
+				values[i] = 1;
 			}
-			senball_calib = senball_calib / 1000;
-			values[i] = senball_calib;
 		}
 		
 		if (i == 8) {
-			values[i] = strres;
+			if (values[i] <= 0) {
+				values[i] = 0;
+			} else {
+				if (values[i] >= 2) {
+					values[i] = 2;
+				}
+			}
 		}
 		
 		if (i == 9) {
-			values[i] = strres;
+			compass_array = i2c.readregs(2, 1, 66, 4);
+        	compass = compass_array[0] * 2 + compass_array[1];
+			values[i] = compass;
+		}
+
+		if (i == 10) {
+			if (values[i] <= 1) {
+				values[i] = 1;
+			}
 		}
 	}
 	
@@ -365,7 +325,7 @@ end: // instead of operator break
 
 scr.clear();
 handle = open.w("cal.txt"); // writing to the file
-for (i = 0; i < 10; ++i) {
+for (i = 0; i < 11; ++i) {
 	writeline(handle, values[i]);
 }
 closef(handle);
--- goalkeeper/goalkeeper.c
+++ goalkeeper/goalkeeper.c
@@ -1,5 +1,192 @@
-void sensors {
-    
+// const values from calibration
+
+handle = open.r("cal.txt"); // open the file with values
+
+PORT_SEEKER = tonum(readline(handle));
+PORT_COMPASS = tonum(readline(handle));
+PORT_LIGHT = tonum(readline(handle));
+PORT_BUTTON = tonum(readline(handle));
+SEEKER_DELAY = tonum(readline(handle));
+COMPASS_DELAY = tonum(readline(handle));
+LIGHT_DELAY = tonum(readline(handle));
+BUTTON_DELAY = tonum(readline(handle));
+LIGHT_COLOR_NUM = tonum(readline(handle));
+COMPASS_ALPHA = tonum(readline(handle));
+SEEKER_DIV = tonum(readline(handle)) / 100;
+
+// reading from sensors start
+
+// light sensor start
+
+LIGHT_COLOR = 'g';
+
+if (LIGHT_COLOR_NUM == 0) {
+    LIGHT_COLOR = 'r';
+} else {
+    if (LIGHT_COLOR_NUM == 1) {
+        LIGHT_COLOR = 'g';
+    } else {
+        if (LIGHT_COLOR_NUM == 2) {
+            LIGHT_COLOR = 'b';
+        }
+    }
 }
 
-new.thread = sensors;
+sen.setmode(PORT_LIGHT, 4);
+
+light_prev_time = time();
+light_prev_value = 0;
+
+func num light() { // get current value of the selected color based on the light sens
+    if (time() - light_prev_time >= LIGHT_DELAY) {
+        local r;
+        local g;
+        local b;
+        local light_val;
+        sen.readrgb(PORT_LIGHT, r, g, b);
+
+        if (LIGHT_COLOR == 'r') {
+            light_val = r;
+        } else { 
+            if (LIGHT_COLOR == 'g') {
+                light_val = g;
+            } else { 
+                if (LIGHT_COLOR == 'b') {
+                    light_val = b;
+                }
+            }
+        }
+
+        light_prev_value = light_val;
+        return light_val;
+    } else {
+       return light_prev_value;
+    }
+
+    light_prev_time = time();
+}
+
+// light sensor end 
+
+// compass start
+
+compass_prev_time = time();
+compass_prev_value = 0;
+
+func num compass() { // get compass current azimut
+    local compass_array;
+    local compass;
+    if (time() - compass_prev_time >= COMPASS_DELAY) {
+        compass_array = i2c.readregs(2, 1, 66, 4);
+        compass = compass_array[0] * 2 + compass_array[1];
+        compass_prev_value = compass;
+        return compass;
+    } else {
+        return compass_prev_value;
+    }
+
+    compass_prev_time = time();
+}
+
+func num compass_delta(compass_value) { // get compass delta based
+    local error = rm(compass_value - COMPASS_ALPHA + 900, 360) - 180;
+    return error;
+}
+
+// compass end
+
+// irseeker start
+
+seeker_prev_time = time();
+seeker_prev_dir = 0;
+seeker_prev_str = 0;
+
+func num irseeker_dir() { // get seeker current dir
+    local irseeker_array;
+    local dir;
+    local str1;
+    local str2;
+    local str3;
+    local str4;
+    local str5;
+
+    if (time() - seeker_prev_time >= SEEKER_DELAY) {
+        irseeker_array = i2c.readregs(4, 8, 73, 6);
+        
+        dir = irseeker_array[0];
+        seeker_prev_dir = dir;
+
+        str1 = irseeker_array[1];
+        str2 = irseeker_array[2];
+        str3 = irseeker_array[3];
+        str4 = irseeker_array[4];
+        str5 = irseeker_array[5];
+
+        local strres = 0;
+        if (rm(dir,2) == 0) {
+            strres = (str1 + str2 + str3 + str4 + str5) / SEEKER_DIV;
+        } else {
+            strres = str1 + str2 + str3 + str4 + str5;
+        }
+        seeker_prev_str = strres;
+
+        return dir;
+    } else {
+        return seeker_prev_dir;
+    }
+
+    seeker_prev_time = time();
+}
+
+func num irseeker_str() { // get seeker current str
+    local irseeker_array;
+    local dir;
+    local str1;
+    local str2;
+    local str3;
+    local str4;
+    local str5;
+
+    if (time() - seeker_prev_time >= SEEKER_DELAY) {
+        irseeker_array = i2c.readregs(4, 8, 73, 6);
+        
+        dir = irseeker_array[0];
+        seeker_prev_dir = dir;
+
+        str1 = irseeker_array[1];
+        str2 = irseeker_array[2];
+        str3 = irseeker_array[3];
+        str4 = irseeker_array[4];
+        str5 = irseeker_array[5];
+
+        local strres = 0;
+        if (rm(dir,2) == 0) {
+            strres = (str1 + str2 + str3 + str4 + str5) / SEEKER_DIV;
+        } else {
+            strres = str1 + str2 + str3 + str4 + str5;
+        }
+        seeker_prev_str = strres;
+
+        return strres;
+    } else {
+        return seeker_prev_str;
+    }
+
+    seeker_prev_time = time();
+}
+
+// irseeker end
+
+// button start
+
+
+
+// button end
+
+// reading from sensors end
+
+// main loop goes here
+
+while (true) {
+    // some code
+}
