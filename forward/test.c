9037a55d1180729483aa36a2f156c16ed7dc3418
VOLEDOL <48492010+VOLEDOL@users.noreply.github.com>
2019-05-16 13:07:57 +0300

    for RRO video

--- forward/test.c
+++ forward/test.c
@@ -204,8 +204,7 @@ void orbit {
         Goto exit2
       } else {
         if (time() - t0 > 1500) {
-          if (strres < 70) {
-            play(100, "Kung Fu")
+          if (strres < 80) {
             Goto exit2
           }
         }
@@ -218,7 +217,7 @@ void orbit {
     //r3
     tone(100,100,100)
     if (err90 < 21) {
-      while (abs(err_com) > 20 and l1 + l2 < l1_cal + l2_cal and block == 0) {
+      while (abs(err_com) > 20 /*and l1 + l2 < l1_cal + l2_cal*/ and block == 0) {
       
         v = 40
         u = -38
@@ -275,7 +274,7 @@ void orbit {
     dir2 = dir1
     dir3 = dir2
 
-    while (err90 < -8) {
+    while (err90 < -8 and block == 0) {
       if (dir > 5) {
         if (dir > 6) {
           err90 = rm(compass - com_l + 900, 360) - 180+40
@@ -305,8 +304,7 @@ void orbit {
         Goto exit2
       } else {
         if (time() - t0 > 1500) {
-          if (strres < 70) {
-            play(100, "Kung Fu")
+          if (strres < 80) {
             Goto exit2
           }
         }
@@ -318,7 +316,7 @@ void orbit {
     //l3
     tone(100,100,100)
     if (err90 > -22) {
-      while (abs(err_com) > 20 and l1 + l2 < l1_cal + l2_cal) {
+      while (abs(err_com) > 20 /*and l1 + l2 < l1_cal + l2_cal*/ and block == 0) {
 
         v = 40
         u = 38
