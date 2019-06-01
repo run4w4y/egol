6c5c1893a567fec998d2a6d2537ce0dbf018af55
VOLEDOL <48492010+VOLEDOL@users.noreply.github.com>
2019-06-01 07:44:08 +0300

    respUTS final (morning)

--- 2forwards/newMM.c
+++ 2forwards/newMM.c
@@ -2,7 +2,20 @@
 
 //  BT
 
-id = new.mailbox("attack_side")
+who_am_i = getname();
+
+if (who_am_i == "BOBA") {
+	who_aint_me = "BIBA";
+} else {
+	who_aint_me = "BOBA";
+}
+
+connect(who_aint_me);
+
+mailbox_biba = new.mailbox("mailbox_biba");
+mailbox_boba = new.mailbox("mailbox_boba");
+mailbox_mode = new.mailbox("mailbox_mode");
+
 
 sen.setmode(3, 1)
 mt.stop("ABC", "false")
@@ -23,6 +36,10 @@ closef(handle);
 //variables
 alpha = COMPASS_ALPHA
 attack = 0
+
+
+mode = 1;
+attack = 0;
 //flags 
 
 //Subs
@@ -56,151 +73,171 @@ void sensors {
     }
 }
 
+void bt {
+	while (true) {
+		if (who_am_i == "BIBA") {
+			mybtstr = attack + "";
+		} else {
+			mybtstr = strres + " " + dir + " " + attack;
+		}
+		
+		if (who_am_i == "BIBA") {
+			bt.send(who_aint_me, "mailbox_biba", mybtstr);
+
+			btstr2 = bt.last(mailbox_boba);
+			res = parse(4, btstr2);
+
+			str_res2 = res[0];
+			dir2 = res[1];
+			attack2 = res[2];
+		} else {
+			bt.send(who_aint_me, "mailbox_boba", mybtstr);
+
+			btstr2 = bt.last(mailbox_biba);
+			res = parse(2, btstr2);
+
+			attack2 = res[0];
+
+			mode = tonum(bt.last(mailbox_mode));
+		}
+		
+		//////////////////////////////////////////////////////////////////////
+		
+		if (who_am_i == "BIBA") {
+            if (abs(strres - str_res2) < 30) {
+                if (abs(dir - 5) == abs(dir2 - 5)) {
+                    if (strres > str_res2) {
+                        mode = 1;
+                    } else {
+                        mode = 0;
+                    }
+                } else {
+                    if (abs(dir - 5) > abs(dir2 - 5)) {
+                        mode = 0;
+                    } else {
+                        mode = 1;
+                    }
+                }
+            } else {
+                if (strres > str_res2) {
+                    mode = 1;
+                } else {
+                    mode = 0;
+                }
+            }
+			bt.send(who_aint_me, "mailbox_mode", abs(mode-1));
+		}
+
+		// buttons will be here
+	}
+}
+
+
 new.thread = sensors
+new.thread = bt
 t = time()
 
 while (true) {
-
-  if (abs(err_com) < 25 and abs(dir - 5) > 3) {
-    pw = 50
-    b_power = 0
-    while (abs(err_com) < 25 and abs(dir - 5) > 3) {
-      if (pw < 100) {
-        pw = pw + 1
-      }
-      // if (time() - t > 1700 and time() - t < 1900) {
-      //   mt.spw("a", -100)
-      // } else {
-      //   mt.stop("a", true)
-      // }
-      b_power = 1 * err_com + err_com*pw*0.01;
-      if (abs(err_com) < 5) {
-        b_power = 0;
-      } else {
-        if (abs(b_power) < 10) {
-          if (b_power > 0) {
-            b_power = 10;
-          } else {
-            b_power = -10;
-          }
+  while (mode == 1) {
+    led(2)
+    if (abs(err_com) < 25 and abs(dir - 5) > 3) {
+      pw = 50
+      b_power = 0
+      while (abs(err_com) < 25 and abs(dir - 5) > 3) {
+        if (pw < 100) {
+          pw = pw + 1
         }
-      }
-
-      u = err_com * 0.008
-      mt.spw("C", -pw+u)
-      mt.spw("D", -pw-u)
-      mt.spw("B", b_power)
-      delay(5)
-      fl1 = 1
-
-      if (l_b > BACK_LIGHT_VALUE) {
-        t_avoid = time()
-        while (time() - t_avoid < 100) {
-          mt.spw("C", 50)
-          mt.spw("D", -33 + err_com)
-          mt.spw("B", 100)
-        }
-      }
-    }
-  }
-
-  //  slowing
-  if (fl1 == 1) { 
-    while (pw > 33) {
-      if (pw > 33) {
-        pw = pw - 3
-      }
-      // if (time() - t > 1700 and time() - t < 1900) {
-      //   mt.spw("a", -100)
-      // } else {
-      //   mt.stop("a", true)
-      // }
-      b_power = 1 * err_com + err_com*pw*0.01;
-      if (abs(err_com) < 5) {
-        b_power = 0;
-      } else {
-        if (abs(b_power) < 10) {
-          if (b_power > 0) {
-            b_power = 10;
-          } else {
-            b_power = -10;
-          }
-        }
-      }
-
-      u = err_com * 0.008
-      mt.spw("C", -pw)
-      mt.spw("D", -pw)
-      mt.spw("B", b_power)
-      delay(5)
-    }
-
-    fl1 = 0
-  }
-
-  while (dir != 5 and abs(dir - 5) < 3) {
-    if (dir > 5) {
-      mt.spw("C", 50)
-      mt.spw("D", -33 + err_com)
-      mt.spw("B", 100)
-    } else {
-      mt.spw("C", -33 - err_com)
-      mt.spw("D", 50)
-      mt.spw("B", -100)
-    }
-  }
-
-  while (dir == 5 and attack == 0) {
-    if (pw < 100) {
-      pw = pw + 1
-    }
-    // if (time() - t > 1700 and time() - t < 1900) {
-    //   mt.spw("a", -100)
-    // } else {
-    //   mt.stop("a", true)
-    // }
-    b_power = 1 * err_com + err_com*pw*0.01;
-    if (abs(err_com) < 5) {
-      b_power = 0;
-    } else {
-      if (abs(b_power) < 10) {
-        if (b_power > 0) {
-          b_power = 10;
+        // if (time() - t > 1700 and time() - t < 1900) {
+        //   mt.spw("a", -100)
+        // } else {
+        //   mt.stop("a", true)
+        // }
+        b_power = 1 * err_com + err_com*pw*0.01;
+        if (abs(err_com) < 5) {
+          b_power = 0;
         } else {
-          b_power = -10;
+          if (abs(b_power) < 10) {
+            if (b_power > 0) {
+              b_power = 10;
+            } else {
+              b_power = -10;
+            }
+          }
+        }
+
+        u = err_com * 0.008
+        mt.spw("C", -pw+u)
+        mt.spw("D", -pw-u)
+        mt.spw("B", b_power)
+        delay(5)
+        fl1 = 1
+
+        if (l_b > BACK_LIGHT_VALUE) {
+          t_avoid = time()
+          while (time() - t_avoid < 100) {
+            mt.spw("C", 50)
+            mt.spw("D", -33 + err_com)
+            mt.spw("B", 100)
+          }
         }
       }
     }
 
-    u = err_com * 0.008
-    mt.spw("C", pw-u)
-    mt.spw("D", pw+u)
-    mt.spw("B", b_power)
-    delay(5)
+    //  slowing
+    if (fl1 == 1) { 
+      while (pw > 33) {
+        if (pw > 33) {
+          pw = pw - 3
+        }
+        // if (time() - t > 1700 and time() - t < 1900) {
+        //   mt.spw("a", -100)
+        // } else {
+        //   mt.stop("a", true)
+        // }
+        b_power = 1 * err_com + err_com*pw*0.01;
+        if (abs(err_com) < 5) {
+          b_power = 0;
+        } else {
+          if (abs(b_power) < 10) {
+            if (b_power > 0) {
+              b_power = 10;
+            } else {
+              b_power = -10;
+            }
+          }
+        }
 
-    if (str3 > 200) {
-      attack = 1
+        u = err_com * 0.008
+        mt.spw("C", -pw)
+        mt.spw("D", -pw)
+        mt.spw("B", b_power)
+        delay(5)
+      }
+
+      fl1 = 0
     }
-  }
 
-  if (attack == 1) {
-    pw = 50
-    b_power = 0
-    t = time()
-    while (strres > 150) {
-      tone(100,100,100)
+    while (dir != 5 and abs(dir - 5) < 3) {
+      if (dir > 5) {
+        mt.spw("C", 50)
+        mt.spw("D", -33 + err_com)
+        mt.spw("B", 100)
+      } else {
+        mt.spw("C", -33 - err_com)
+        mt.spw("D", 50)
+        mt.spw("B", -100)
+      }
+    }
+
+    while (dir == 5 and attack == 0) {
       if (pw < 100) {
         pw = pw + 1
       }
-
-      if (time() - t > 1000 and time() - t < 1200) {
-        mt.spw("a", -100)
-      } else {
-        mt.shd.pw("a", 50, 0, 300, 0, "false")
-      }
-
-      
-
+      // if (time() - t > 1700 and time() - t < 1900) {
+      //   mt.spw("a", -100)
+      // } else {
+      //   mt.stop("a", true)
+      // }
       b_power = 1 * err_com + err_com*pw*0.01;
       if (abs(err_com) < 5) {
         b_power = 0;
@@ -215,11 +252,140 @@ while (true) {
       }
 
       u = err_com * 0.008
-      mt.spw("C", pw)
-      mt.spw("D", pw)
+      mt.spw("C", pw-u)
+      mt.spw("D", pw+u)
       mt.spw("B", b_power)
       delay(5)
+
+      if (str3 > 200) {
+        attack = 1
+      }
     }
-    attack = 0
+
+    if (attack == 1) {
+      pw = 50
+      b_power = 0
+      t = time()
+      while (strres > 150) {
+        tone(100,100,100)
+        if (pw < 100) {
+          pw = pw + 1
+        }
+
+        if (time() - t > 1000 and time() - t < 1200) {
+          mt.spw("a", -100)
+        } else {
+          mt.shd.pw("a", 50, 0, 300, 0, "false")
+        }
+
+        
+
+        b_power = 1 * err_com + err_com*pw*0.01;
+        if (abs(err_com) < 5) {
+          b_power = 0;
+        } else {
+          if (abs(b_power) < 10) {
+            if (b_power > 0) {
+              b_power = 10;
+            } else {
+              b_power = -10;
+            }
+          }
+        }
+
+        u = err_com * 0.008
+        mt.spw("C", pw)
+        mt.spw("D", pw)
+        mt.spw("B", b_power)
+        delay(5)
+      }
+      attack = 0
+    }
+  }
+
+  while (mode == 0) {
+    led(1)
+    if (abs(dir - 5) < 3) {
+      mt.spw("C", -err_com * 0.008)
+      mt.spw("D", err_com * 0.008)
+      mt.spw("B", err_com * 0.01)
+    } else {
+      if (abs(err_com) < 25 and abs(dir - 5) > 3) {
+        pw = 50
+        b_power = 0
+        while (abs(err_com) < 25 and abs(dir - 5) > 3) {
+          if (pw < 100) {
+            pw = pw + 1
+          }
+          // if (time() - t > 1700 and time() - t < 1900) {
+          //   mt.spw("a", -100)
+          // } else {
+          //   mt.stop("a", true)
+          // }
+          b_power = 1 * err_com + err_com*pw*0.01;
+          if (abs(err_com) < 5) {
+            b_power = 0;
+          } else {
+            if (abs(b_power) < 10) {
+              if (b_power > 0) {
+                b_power = 10;
+              } else {
+                b_power = -10;
+              }
+            }
+          }
+
+          u = err_com * 0.008
+          mt.spw("C", -pw+u)
+          mt.spw("D", -pw-u)
+          mt.spw("B", b_power)
+          delay(5)
+          fl2 = 1
+
+          if (l_b > BACK_LIGHT_VALUE) {
+            t_avoid = time()
+            while (time() - t_avoid < 100) {
+              mt.spw("C", 50)
+              mt.spw("D", -33 + err_com)
+              mt.spw("B", 100)
+            }
+          }
+        }
+      }
+
+      //  slowing
+      if (fl2 == 1) { 
+        while (pw > 33) {
+          if (pw > 33) {
+            pw = pw - 3
+          }
+          // if (time() - t > 1700 and time() - t < 1900) {
+          //   mt.spw("a", -100)
+          // } else {
+          //   mt.stop("a", true)
+          // }
+          b_power = 1 * err_com + err_com*pw*0.01;
+          if (abs(err_com) < 5) {
+            b_power = 0;
+          } else {
+            if (abs(b_power) < 10) {
+              if (b_power > 0) {
+                b_power = 10;
+              } else {
+                b_power = -10;
+              }
+            }
+          }
+
+          u = err_com * 0.008
+          mt.spw("C", -pw)
+          mt.spw("D", -pw)
+          mt.spw("B", b_power)
+          delay(5)
+        }
+
+        fl2 = 0
+      }
+    }
   }
 }
